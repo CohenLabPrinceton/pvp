2a655c0838da8a60040d33f3decc2737c35225cd
zhenyus <sunnyszy@gmail.com>
2020-04-17 09:54:31 -0400

    fix: change multi_process parameter to be single_process

--- vent/main.py
+++ vent/main.py
@@ -8,13 +8,13 @@ def parse_cmd_args():
     # TODO: maybe we should add a mode without UI display, so this would only have command line interface?
     parser.add_argument('--simulation', help='running as simulation using virtual sensors and actuators',
                         action='store_true')
-    parser.add_argument('--multi_process', help='running UI and controller as separate processes', action='store_true')
+    parser.add_argument('--single_process', help='running UI and controller within one process', action='store_true')
     return parser.parse_args()
 
 
 def main():
     args = parse_cmd_args()
-    ui_control_module = get_ui_control_module(single_process=args.multi_process, sim_mode=args.simulation)
+    ui_control_module = get_ui_control_module(single_process=args.single_process, sim_mode=args.simulation)
     # TODO: gui.main(ui_control_module)
 
 

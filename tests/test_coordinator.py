e884a58d13de64a1f7f5041ec1671db13978800d
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2020-04-23 19:25:17 -0400

    try this

--- tests/test_coordinator.py
+++ tests/test_coordinator.py
@@ -13,27 +13,26 @@ import random
                                                   ControlSettingName.BREATHS_PER_MINUTE,
                                                   ControlSettingName.INSPIRATION_TIME_SEC])
 
-# def test_single_process_simulation(control_setting_name):
-#     coordinator = get_coordinator(single_process=True, sim_mode=True)
-#     t = time.time()
-#     v = random.randint(10, 100)
-#     v_min = v - 5
-#     v_max = v + 5
+def test_single_process_simulation(control_setting_name):
+    coordinator = get_coordinator(single_process=True, sim_mode=True)
+    t = time.time()
+    v = random.randint(10, 100)
+    v_min = v - 5
+    v_max = v + 5
 
-#     c = ControlSetting(name=control_setting_name, value=v, min_value=v_min, max_value=v_max, timestamp=t)
-#     coordinator.set_control(c)
+    c = ControlSetting(name=control_setting_name, value=v, min_value=v_min, max_value=v_max, timestamp=t)
+    coordinator.set_control(c)
+    assert True
 
-#     c_read = coordinator.get_control(control_setting_name)
-    
+#     c_read = coordinator.get_control(control_setting_name)  
 #     assert c_read.name == c.name
 #     assert c_read.value == c.value
     # assert c_read.min_value == c.min_value
     # assert c_read.max_value == c.max_value
     # assert c_read.timestamp == c.timestamp
 
-
 def test_controller():
-    #Simple test for controller
+
     Controller = get_control_module(sim_mode=True)
 
     Controller.start()

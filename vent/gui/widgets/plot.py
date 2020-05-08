c0c84128f140570e128079e089fa2de4b56b0503
sneakers-the-rat <JLSaunders987@gmail.com>
2020-05-08 00:18:02 -0700

    commenting some unused objects, will remove when full profiling is done.

--- vent/gui/widgets/plot.py
+++ vent/gui/widgets/plot.py
@@ -158,144 +158,144 @@ class Plot(pg.PlotWidget):
     #     self.addItem(item, params=params)
     #
     #     return item
-
-class TimedPlotDataItem(pg.PlotDataItem):
-    """
-    Subclass of :class:`pyqtgraph.PlotDataItem` that uses :class:`.TimedPlotCurveItem`
-
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(TimedPlotDataItem, self).__init__(*args, **kwargs)
-
-        # replace self.curve with timed version
-        # mimic __init__ from parent
-        # https://github.com/pyqtgraph/pyqtgraph/blob/a2053b13d0234e210561a73fa044625fd972e910/pyqtgraph/graphicsItems/PlotDataItem.py#L37
-        self.curve = TimedPlotCurveItem()
-        self.curve.setParentItem(self)
-        self.curve.sigClicked.connect(self.curveClicked)
-
-        self.setData(*args, **kwargs)
-
-class TimedPlotCurveItem(pg.PlotCurveItem):
-    """
-    Subclass :class:`pyqtgraph.PlotCurveItem` to update with a fixed frequency instead of whenever new data pushed
-
-    Either creates or gets :data:`PLOT_TIMER` and connects it to :meth:`TimedPlotCurveItem.update`
-    """
-
-
-    def __init__(self, *args, **kwargs):
-        super(TimedPlotCurveItem, self).__init__(*args, **kwargs)
-
-        if globals()['PLOT_TIMER'] is None:
-            globals()['PLOT_TIMER'] = QtCore.QTimer()
-
-            get_gui_instance().gui_closing.connect(globals()['PLOT_TIMER'].stop)
-
-        self.timer = globals()['PLOT_TIMER']
-        # stop timer, add ourselves, restart
-        self.timer.timeout.connect(self.update)
-
-        try:
-            self.fps = float(globals()['PLOT_FREQ'])
-        except (ValueError, KeyError):
-            self.fps = 15.
-
-        self.timer.start(1./self.fps)
-
-
-
-    def __del__(self):
-        """
-        On object deletion, remove from :data:`.PLOT_TIMER` 's signals
-        """
-        #self.timer.disconnect(self.update)
-        #super(TimedPlotCurveItem, self).__del__()
-
-
-    def updateData(self, *args, **kargs):
-        """
-        Override :meth:`pyqtgraph.PlotCurveItem.updateData` to omit call to :meth:`.update`
-        which is done by :data:`.PLOT_TIMER`
-
-        """
-        profiler = pg.debug.Profiler()
-
-        if 'compositionMode' in kargs:
-            self.setCompositionMode(kargs['compositionMode'])
-
-        if len(args) == 1:
-            kargs['y'] = args[0]
-        elif len(args) == 2:
-            kargs['x'] = args[0]
-            kargs['y'] = args[1]
-
-        if 'y' not in kargs or kargs['y'] is None:
-            kargs['y'] = np.array([])
-        if 'x' not in kargs or kargs['x'] is None:
-            kargs['x'] = np.arange(len(kargs['y']))
-
-        for k in ['x', 'y']:
-            data = kargs[k]
-            if isinstance(data, list):
-                data = np.array(data)
-                kargs[k] = data
-            if not isinstance(data, np.ndarray) or data.ndim > 1:
-                raise Exception("Plot data must be 1D ndarray.")
-            if data.dtype.kind == 'c':
-                raise Exception("Can not plot complex data types.")
-
-        profiler("data checks")
-
-        # self.setCacheMode(QtGui.QGraphicsItem.NoCache)  ## Disabling and re-enabling the cache works around a bug in Qt 4.6 causing the cached results to display incorrectly
-        ##    Test this bug with test_PlotWidget and zoom in on the animated plot
-        self.yData = kargs['y'].view(np.ndarray)
-        self.xData = kargs['x'].view(np.ndarray)
-
-        self.invalidateBounds()
-        self.prepareGeometryChange()
-        self.informViewBoundsChanged()
-
-        profiler('copy')
-
-        if 'stepMode' in kargs:
-            self.opts['stepMode'] = kargs['stepMode']
-
-        if self.opts['stepMode'] is True:
-            if len(self.xData) != len(self.yData) + 1:  ## allow difference of 1 for step mode plots
-                raise Exception("len(X) must be len(Y)+1 since stepMode=True (got %s and %s)" % (
-                self.xData.shape, self.yData.shape))
-        else:
-            if self.xData.shape != self.yData.shape:  ## allow difference of 1 for step mode plots
-                raise Exception(
-                    "X and Y arrays must be the same shape--got %s and %s." % (self.xData.shape, self.yData.shape))
-
-        self.path = None
-        self.fillPath = None
-        self._mouseShape = None
-        # self.xDisp = self.yDisp = None
-
-        if 'name' in kargs:
-            self.opts['name'] = kargs['name']
-        if 'connect' in kargs:
-            self.opts['connect'] = kargs['connect']
-        if 'pen' in kargs:
-            self.setPen(kargs['pen'])
-        if 'shadowPen' in kargs:
-            self.setShadowPen(kargs['shadowPen'])
-        if 'fillLevel' in kargs:
-            self.setFillLevel(kargs['fillLevel'])
-        if 'fillOutline' in kargs:
-            self.opts['fillOutline'] = kargs['fillOutline']
-        if 'brush' in kargs:
-            self.setBrush(kargs['brush'])
-        if 'antialias' in kargs:
-            self.opts['antialias'] = kargs['antialias']
-
-        profiler('set')
-        #self.update()
-        profiler('update')
-        #self.sigPlotChanged.emit(self)
-        profiler('emit')
+#
+# class TimedPlotDataItem(pg.PlotDataItem):
+#     """
+#     Subclass of :class:`pyqtgraph.PlotDataItem` that uses :class:`.TimedPlotCurveItem`
+#
+#     """
+#
+#     def __init__(self, *args, **kwargs):
+#         super(TimedPlotDataItem, self).__init__(*args, **kwargs)
+#
+#         # replace self.curve with timed version
+#         # mimic __init__ from parent
+#         # https://github.com/pyqtgraph/pyqtgraph/blob/a2053b13d0234e210561a73fa044625fd972e910/pyqtgraph/graphicsItems/PlotDataItem.py#L37
+#         self.curve = TimedPlotCurveItem()
+#         self.curve.setParentItem(self)
+#         self.curve.sigClicked.connect(self.curveClicked)
+#
+#         self.setData(*args, **kwargs)
+#
+# class TimedPlotCurveItem(pg.PlotCurveItem):
+#     """
+#     Subclass :class:`pyqtgraph.PlotCurveItem` to update with a fixed frequency instead of whenever new data pushed
+#
+#     Either creates or gets :data:`PLOT_TIMER` and connects it to :meth:`TimedPlotCurveItem.update`
+#     """
+#
+#
+#     def __init__(self, *args, **kwargs):
+#         super(TimedPlotCurveItem, self).__init__(*args, **kwargs)
+#
+#         if globals()['PLOT_TIMER'] is None:
+#             globals()['PLOT_TIMER'] = QtCore.QTimer()
+#
+#             get_gui_instance().gui_closing.connect(globals()['PLOT_TIMER'].stop)
+#
+#         self.timer = globals()['PLOT_TIMER']
+#         # stop timer, add ourselves, restart
+#         self.timer.timeout.connect(self.update)
+#
+#         try:
+#             self.fps = float(globals()['PLOT_FREQ'])
+#         except (ValueError, KeyError):
+#             self.fps = 15.
+#
+#         self.timer.start(1./self.fps)
+#
+#
+#
+#     def __del__(self):
+#         """
+#         On object deletion, remove from :data:`.PLOT_TIMER` 's signals
+#         """
+#         #self.timer.disconnect(self.update)
+#         #super(TimedPlotCurveItem, self).__del__()
+#
+#
+#     def updateData(self, *args, **kargs):
+#         """
+#         Override :meth:`pyqtgraph.PlotCurveItem.updateData` to omit call to :meth:`.update`
+#         which is done by :data:`.PLOT_TIMER`
+#
+#         """
+#         profiler = pg.debug.Profiler()
+#
+#         if 'compositionMode' in kargs:
+#             self.setCompositionMode(kargs['compositionMode'])
+#
+#         if len(args) == 1:
+#             kargs['y'] = args[0]
+#         elif len(args) == 2:
+#             kargs['x'] = args[0]
+#             kargs['y'] = args[1]
+#
+#         if 'y' not in kargs or kargs['y'] is None:
+#             kargs['y'] = np.array([])
+#         if 'x' not in kargs or kargs['x'] is None:
+#             kargs['x'] = np.arange(len(kargs['y']))
+#
+#         for k in ['x', 'y']:
+#             data = kargs[k]
+#             if isinstance(data, list):
+#                 data = np.array(data)
+#                 kargs[k] = data
+#             if not isinstance(data, np.ndarray) or data.ndim > 1:
+#                 raise Exception("Plot data must be 1D ndarray.")
+#             if data.dtype.kind == 'c':
+#                 raise Exception("Can not plot complex data types.")
+#
+#         profiler("data checks")
+#
+#         # self.setCacheMode(QtGui.QGraphicsItem.NoCache)  ## Disabling and re-enabling the cache works around a bug in Qt 4.6 causing the cached results to display incorrectly
+#         ##    Test this bug with test_PlotWidget and zoom in on the animated plot
+#         self.yData = kargs['y'].view(np.ndarray)
+#         self.xData = kargs['x'].view(np.ndarray)
+#
+#         self.invalidateBounds()
+#         self.prepareGeometryChange()
+#         self.informViewBoundsChanged()
+#
+#         profiler('copy')
+#
+#         if 'stepMode' in kargs:
+#             self.opts['stepMode'] = kargs['stepMode']
+#
+#         if self.opts['stepMode'] is True:
+#             if len(self.xData) != len(self.yData) + 1:  ## allow difference of 1 for step mode plots
+#                 raise Exception("len(X) must be len(Y)+1 since stepMode=True (got %s and %s)" % (
+#                 self.xData.shape, self.yData.shape))
+#         else:
+#             if self.xData.shape != self.yData.shape:  ## allow difference of 1 for step mode plots
+#                 raise Exception(
+#                     "X and Y arrays must be the same shape--got %s and %s." % (self.xData.shape, self.yData.shape))
+#
+#         self.path = None
+#         self.fillPath = None
+#         self._mouseShape = None
+#         # self.xDisp = self.yDisp = None
+#
+#         if 'name' in kargs:
+#             self.opts['name'] = kargs['name']
+#         if 'connect' in kargs:
+#             self.opts['connect'] = kargs['connect']
+#         if 'pen' in kargs:
+#             self.setPen(kargs['pen'])
+#         if 'shadowPen' in kargs:
+#             self.setShadowPen(kargs['shadowPen'])
+#         if 'fillLevel' in kargs:
+#             self.setFillLevel(kargs['fillLevel'])
+#         if 'fillOutline' in kargs:
+#             self.opts['fillOutline'] = kargs['fillOutline']
+#         if 'brush' in kargs:
+#             self.setBrush(kargs['brush'])
+#         if 'antialias' in kargs:
+#             self.opts['antialias'] = kargs['antialias']
+#
+#         profiler('set')
+#         #self.update()
+#         profiler('update')
+#         #self.sigPlotChanged.emit(self)
+#         profiler('emit')
 

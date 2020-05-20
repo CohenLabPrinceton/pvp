b95940dcfa1b30fd566c58a838b840b4181e5650
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2020-05-20 13:28:23 -0400

    dont save per default

--- vent/controller/control_module.py
+++ vent/controller/control_module.py
@@ -49,7 +49,7 @@ class ControlModuleBase:
         self._LOOP_UPDATE_TIME                   = 0.01    # Run the main control loop every 0.01 sec
         self._NUMBER_CONTROLL_LOOPS_UNTIL_UPDATE = 10      # After every 10 main control loop iterations, update COPYs.
         self._RINGBUFFER_SIZE                    = 100     # Maximum number of breath cycles kept in memory
-        self._save_logs                          = True    # Keep logs in a file
+        self._save_logs                          = False   # Keep logs in a file
 
         #########################  Control management  #########################
 
@@ -391,7 +391,7 @@ class ControlModuleBase:
 
         if self._save_logs:
             self.dl.store_control_command(control_setting)
-            
+
         self._lock.release()
 
     def get_control(self, control_setting_name: ValueName) -> ControlSetting:

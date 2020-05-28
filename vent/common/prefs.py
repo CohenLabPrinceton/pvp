e91ba9a5dc2d507e91cdeeb51646aa037ae94f6d
sneakers-the-rat <JLSaunders987@gmail.com>
2020-05-27 17:19:47 -0700

    init prefs in package root

--- vent/common/prefs.py
+++ vent/common/prefs.py
@@ -1,8 +1,18 @@
-VENT_DIR = None
-LOG_DIR = None
-DATA_DIR = None
+import os
 
 _CONFIG = {}
+
+_DIRECTORIES = {}
+"""
+Directories to ensure are created and added to prefs.
+
+    * ``VENT_DIR``: ~/vent - base directory for user storage
+    * ``LOG_DIR``: ~/vent/logs - for storage of event and alarm logs
+    * ``DATA_DIR``: ~/vent/data - for storage of waveform data
+"""
+_DIRECTORIES['VENT_DIR'] = os.path.join(os.path.expanduser('~'), 'vent')
+_DIRECTORIES['LOG_DIR'] = os.path.join(_DIRECTORIES['VENT_DIR'], 'logs')
+_DIRECTORIES['DATA_DIR'] = os.path.join(_DIRECTORIES['DATA_DIR'], 'logs')
 
 def set_pref(key: str, val):
     globals()['_CONFIG'][key] = val
@@ -22,3 +32,26 @@ def get_pref(key: str = None):
             return globals()['_CONFIG'][key]
         except KeyError:
             return None
+
+
+def make_dirs():
+    """
+    ensures _DIRECTORIES are created and added to prefs.
+    """
+    global _DIRECTORIES
+    # create directories if they don't exist already
+    for dir_name, make_dir in _DIRECTORIES.items():
+        if not os.path.exists(make_dir):
+            os.mkdir(make_dir)
+
+        set_pref(dir_name, make_dir)
+
+def init():
+    """
+    Initialize prefs. Called in ``vent.__init__.py`` to ensure prefs are initialized before anything else.
+    """
+
+    # add more functions as needed, but probably bad to hardcode default prefs here.
+    # pull them up top like _DIRECTORIES
+
+    make_dirs()
--- vent/main.py
+++ vent/main.py
@@ -18,36 +18,8 @@ def parse_cmd_args():
                         action='store_true')
     return parser.parse_args()
 
-def make_vent_dirs():
-    """
-    Make a directory to store logs, data, and user configuration in ``<user director>/vent``
-
-    Creates::
-
-        ~/vent
-        ~/vent/logs - for storage of event and alarm logs
-        ~/vent/data - for storage of waveform data
-    """
-
-    # root, log, and data vent directories
-    vent_dir = os.path.join(os.path.expanduser('~'), 'vent')
-    log_dir = os.path.join(vent_dir, 'logs')
-    data_dir = os.path.join(vent_dir, 'data')
-
-    # create directories if they don't exist already
-    for make_dir in (vent_dir, log_dir, data_dir):
-        if not os.path.exists(make_dir):
-            os.mkdir(make_dir)
-
-    # store them as config values
-    prefs.set_pref('VENT_DIR', vent_dir)
-    prefs.set_pref('LOG_DIR', log_dir)
-    prefs.set_pref('DATA_DIR', data_dir)
-
-
 
 def main():
-    make_vent_dirs()
     args = parse_cmd_args()
     coordinator = get_coordinator(single_process=args.single_process, sim_mode=args.simulation)
     app, gui = launch_gui(coordinator)

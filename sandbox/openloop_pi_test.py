3f896c31cd21420e9f78a72ac11d0d7fa31fa70c
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2020-06-01 16:32:11 -0400

    Update openloop_pi_test.py

--- sandbox/openloop_pi_test.py
+++ sandbox/openloop_pi_test.py
@@ -20,8 +20,7 @@ def cycle(idx, store_len):
     # Ramp up to PIP over inhale time
     for i in range(n_ramp_steps+1):
         # Get "valve openness" setpoint
-        # setpnt = int(120 / n_ramp_steps * i)
-        setpnt = int(80 + 20*i/n_ramp_steps)
+        setpnt = int(50 / n_ramp_steps * i)
         print(np.round(setpnt, 2))
         
         # Set duty cycle via response curve in hardware abstraction layer ("hal")

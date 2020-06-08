17633fc6ccc5f174157e0d5d88295530c8e70602
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2020-06-08 10:56:52 -0400

    peep to 8

--- sandbox/start_Controller_pi.py
+++ sandbox/start_Controller_pi.py
@@ -25,7 +25,7 @@ def do_stuff():
             print(t)
 
         ## Do things
-        command = ControlSetting(name=ValueName.PEEP, value=10)
+        command = ControlSetting(name=ValueName.PEEP, value=8)
         Controller.set_control(command)
         command = ControlSetting(name=ValueName.PIP, value=25)
         Controller.set_control(command)

7633faea3073b8a58a5cd9426d3330fdc5b5c327
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2020-06-08 16:41:46 -0400

    no thread lock

--- vent/controller/control_module.py
+++ vent/controller/control_module.py
@@ -806,12 +806,11 @@ class ControlModuleDevice(ControlModuleBase):
         """
         Get sensor values from HAL, decorated with timeout
         """
-        with self._running.is_set():
-            self._DATA_PRESSURE = self.HAL.pressure
-            time.sleep(0.01)
-            self._DATA_Qout = 0                         # current hardware does not support that.
-            self._DATA_Qin  = self.HAL.flow_ex          # "flow_ex" is the low out of the system. VTE is derived from the integral of this quantity.
-            time.sleep(0.01)
+        self._DATA_PRESSURE = self.HAL.pressure
+        time.sleep(0.01)
+        self._DATA_Qout = 0                         # current hardware does not support that.
+        self._DATA_Qin  = self.HAL.flow_ex          # "flow_ex" is the low out of the system. VTE is derived from the integral of this quantity.
+        time.sleep(0.01)
 
 
     def _start_mainloop(self):

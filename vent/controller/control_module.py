e2207a2576ead60d09d004d105a88a0ed67701b2
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2020-06-08 11:36:07 -0400

    removed flow

--- vent/controller/control_module.py
+++ vent/controller/control_module.py
@@ -806,8 +806,8 @@ class ControlModuleDevice(ControlModuleBase):
         """
         Get sensor values from HAL, decorated with timeout
         """
-        self._DATA_Qout = self.HAL.flow_ex                      # Flow sensor on Expiratory side
-        self._DATA_Qin  = self.HAL.flow_in                      # Flow sensor on inspiratory side. NOTE: used to calculate VTE
+        self._DATA_Qout = 0 #self.HAL.flow_ex                      # Flow sensor on Expiratory side   NOTE: THIS DOESNT WORK RN!
+        self._DATA_Qin  = 0# self.HAL.flow_in                      # Flow sensor on inspiratory side. NOTE: used to calculate VTE
         self._DATA_PRESSURE = self.HAL.pressure
 
     def _start_mainloop(self):

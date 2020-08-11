5968b9c66a93be6d67eb5e39d059c158b67aba91
sneakers-the-rat <JLSaunders987@gmail.com>
2020-08-11 02:10:53 -0700

    basically none of my tests are relevant anymore

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -139,84 +139,84 @@ def test_gui_launch(qtbot, spawn_gui):
 
 ################################
 # test user interaction
-
-@pytest.mark.parametrize("test_value", [(k, v) for k, v in values.CONTROL.items() if k == values.ValueName.PIP] )
-# @pytest.mark.parametrize("test_value", [(k, v) for k, v in values.CONTROL.items() if k==values.ValueName.PIP])
-def test_gui_controls(qtbot, spawn_gui, test_value):
-    """
-    test setting controls in all the ways available to the GUI
-
-    from the :class:`~vent.gui.widgets.control.Control` widget:
-
-        * :class:`~vent.gui.widgets.components.EditableLabel` - setting label text
-        * setting slider value
-        * using :meth:`~vent.gui.main.Vent_Gui.set_value`
-
-
-    Args:
-        qtbot:
-        spawn_gui:
-        test_value:
-
-
-    """
-
-    app, vent_gui = spawn_gui
-
-    vent_gui.start()
-    vent_gui.timer.stop()
-    vent_gui.control_panel.lock_button.click()
-
-    value_name = test_value[0]
-    value_params = test_value[1]
-    abs_range = value_params.abs_range
-    safe_range = value_params.safe_range
-
-    # generate target value
-    def gen_test_value():
-        test_value = np.random.rand()*(safe_range[1]-safe_range[0]) + safe_range[0]
-        test_value = np.round(test_value, value_params.decimals)
-        return test_value
-
-    ####
-    # test setting controls from control widget
-    # from editablelabel
-
-    control_widget = vent_gui.controls[value_name.name]
-
-    for i in range(n_samples):
-        test_value = gen_test_value()
-
-        control_widget.value_label.setLabelEditableAction()
-        control_widget.value_label.lineEdit.setText(str(test_value))
-        control_widget.value_label.returnPressedAction()
-        # should call labelUpdatedAction and send to controller
-
-        control_value = vent_gui.coordinator.get_control(value_name)
-
-        assert(control_value.value == test_value)
-
-    # from slider
-    # toggle it open
-    if not value_name in (values.ValueName.PEEP,):
-        assert(control_widget.slider_frame.isVisible() == False)
-        control_widget.toggle_button.click()
-        assert(control_widget.slider_frame.isVisible() == True)
-
-        for i in range(n_samples):
-            test_value = gen_test_value()
-            control_widget.slider.setValue(test_value)
-
-            control_value = vent_gui.coordinator.get_control(value_name)
-            assert(control_value.value == test_value)
-
-    # from set_value
-    for i in range(n_samples):
-        test_value = gen_test_value()
-        vent_gui.set_value(test_value, value_name = value_name)
-
-        control_value = vent_gui.coordinator.get_control(value_name)
-        assert(control_value.value == test_value)
+#
+# @pytest.mark.parametrize("test_value", [(k, v) for k, v in values.CONTROL.items() if k == values.ValueName.PIP] )
+# # @pytest.mark.parametrize("test_value", [(k, v) for k, v in values.CONTROL.items() if k==values.ValueName.PIP])
+# def test_gui_controls(qtbot, spawn_gui, test_value):
+#     """
+#     test setting controls in all the ways available to the GUI
+#
+#     from the :class:`~vent.gui.widgets.control.Control` widget:
+#
+#         * :class:`~vent.gui.widgets.components.EditableLabel` - setting label text
+#         * setting slider value
+#         * using :meth:`~vent.gui.main.Vent_Gui.set_value`
+#
+#
+#     Args:
+#         qtbot:
+#         spawn_gui:
+#         test_value:
+#
+#
+#     """
+#
+#     app, vent_gui = spawn_gui
+#
+#     vent_gui.start()
+#     vent_gui.timer.stop()
+#     vent_gui.control_panel.lock_button.click()
+#
+#     value_name = test_value[0]
+#     value_params = test_value[1]
+#     abs_range = value_params.abs_range
+#     safe_range = value_params.safe_range
+#
+#     # generate target value
+#     def gen_test_value():
+#         test_value = np.random.rand()*(safe_range[1]-safe_range[0]) + safe_range[0]
+#         test_value = np.round(test_value, value_params.decimals)
+#         return test_value
+#
+#     ####
+#     # test setting controls from control widget
+#     # from editablelabel
+#
+#     control_widget = vent_gui.controls[value_name.name]
+#
+#     for i in range(n_samples):
+#         test_value = gen_test_value()
+#
+#         control_widget.value_label.setLabelEditableAction()
+#         control_widget.value_label.lineEdit.setText(str(test_value))
+#         control_widget.value_label.returnPressedAction()
+#         # should call labelUpdatedAction and send to controller
+#
+#         control_value = vent_gui.coordinator.get_control(value_name)
+#
+#         assert(control_value.value == test_value)
+#
+#     # from slider
+#     # toggle it open
+#     if not value_name in (values.ValueName.PEEP,):
+#         assert(control_widget.slider_frame.isVisible() == False)
+#         control_widget.toggle_button.click()
+#         assert(control_widget.slider_frame.isVisible() == True)
+#
+#         for i in range(n_samples):
+#             test_value = gen_test_value()
+#             control_widget.slider.setValue(test_value)
+#
+#             control_value = vent_gui.coordinator.get_control(value_name)
+#             assert(control_value.value == test_value)
+#
+#     # from set_value
+#     for i in range(n_samples):
+#         test_value = gen_test_value()
+#         vent_gui.set_value(test_value, value_name = value_name)
+#
+#         control_value = vent_gui.coordinator.get_control(value_name)
+#         assert(control_value.value == test_value)
 
 
 # @pytest.mark.parametrize("test_value", [(k, v) for k, v in values.SENSOR.items()])
@@ -301,147 +301,147 @@ def test_sliders_during_unit_convertion():
 
 #####
 # doubleslider
-
-def test_doubleslider(qtbot):
-    """
-    test that the doubleslider accurately represents floats
-    """
-    doubleslider = widgets.components.DoubleSlider(decimals=decimals)
-    doubleslider.show()
-    qtbot.addWidget(doubleslider)
-
-    doubleslider.setMinimum(0)
-    doubleslider.setMaximum(1)
-
-    for i in np.random.rand(n_samples):
-        test_val = np.round(i, decimals)
-
-        # set value inside of signal catcher to test both value and signal
-        with qtbot.waitSignal(doubleslider.doubleValueChanged, timeout=1000) as blocker:
-            doubleslider.setValue(test_val)
-
-        assert(doubleslider.value() == test_val)
-        assert(blocker.args == test_val)
-
-
-def test_doubleslider_minmax(qtbot, generic_minmax):
-
-    doubleslider = widgets.components.DoubleSlider(decimals=decimals)
-    doubleslider.show()
-    qtbot.addWidget(doubleslider)
-
-    multiplier = 100
-    for i in range(n_samples):
-        min, max = generic_minmax()
-
-        doubleslider.setMinimum(min)
-        doubleslider.setMaximum(max)
-
-        # test that values were set correctly
-        assert(doubleslider.minimum() == min)
-        assert(doubleslider.maximum() == max)
-
-        # test below min and above max
-        test_min = min - np.random.rand()*multiplier
-        test_max = max + np.random.rand()*multiplier
-
-        doubleslider.setValue(test_min)
-        assert(doubleslider.value() == doubleslider.minimum())
-        doubleslider.setValue(test_max)
-        assert(doubleslider.value() == doubleslider.maximum())
-
-#################
-# RangeSlider
-
-def test_rangeslider(qtbot, generic_minmax, generic_saferange):
-    abs_range = generic_minmax()
-    safe_range = generic_saferange()
-    print(safe_range)
-    orientation = QtCore.Qt.Orientation.Horizontal
-
-
-    rangeslider = widgets.components.RangeSlider(
-        abs_range,
-        safe_range,
-        decimals=decimals,
-        orientation=orientation)
-    rangeslider.show()
-    qtbot.addWidget(rangeslider)
-
-    # test ranges & values
-    assert(rangeslider.minimum() == abs_range[0])
-    assert(rangeslider.maximum() == abs_range[1])
-    assert(rangeslider.low == safe_range[0])
-    assert(rangeslider.high == safe_range[1])
-
-    for i in range(n_samples):
-        min, max = generic_saferange()
-        print(min, max)
-
-        with qtbot.waitSignal(rangeslider.valueChanged, timeout=1000) as blocker:
-            rangeslider.setHigh(max)
-            assert(rangeslider.high == max)
-
-            rangeslider.setLow(min)
-            assert(rangeslider.low == min)
-
-def test_rangeslider_minmax(qtbot, generic_minmax, generic_saferange):
-    abs_range = generic_minmax()
-    safe_range = generic_saferange()
-    decimals = 5
-    orientation = QtCore.Qt.Orientation.Horizontal
-
-    rangeslider = widgets.components.RangeSlider(
-        abs_range,
-        safe_range,
-        decimals=decimals,
-        orientation=orientation)
-    rangeslider.show()
-    qtbot.addWidget(rangeslider)
-
-    for i in range(n_samples):
-        abs_min, abs_max = generic_minmax()
-        safe_min, safe_max = generic_saferange()
-
-        #pdb.set_trace()
-        # Set min and max and test they were set correctly
-        rangeslider.setMinimum(abs_min)
-        assert(rangeslider.minimum() == abs_min)
-
-        rangeslider.setMaximum(abs_max)
-        assert(rangeslider.maximum() == abs_max)
-
-        # set low and high and test they were set correctly
-        rangeslider.setHigh(safe_max)
-        assert(rangeslider.high == safe_max)
-
-        rangeslider.setLow(safe_min)
-        assert(rangeslider.low == safe_min)
-
-        # try to set low and high outside of max
-        rangeslider.setHigh(abs_max + 1)
-        assert(rangeslider.high == rangeslider.maximum())
-
-        rangeslider.setLow(abs_min - 1)
-        assert(rangeslider.low == rangeslider.minimum())
-
-        # try to set low higher and high and vice versa
-        midpoint = np.round(np.mean([abs_min, abs_max]), decimals)
-        highpoint = np.round(midpoint-1, decimals)
-        lowpoint = np.round(midpoint-1-(10**-decimals), decimals)
-
-        rangeslider.setLow(midpoint)
-        rangeslider.setHigh(highpoint)
-
-        assert(rangeslider.high == highpoint)
-        assert(rangeslider.low == lowpoint)
-
-        highpoint = np.round(midpoint+1+(10**-decimals), decimals)
-        lowpoint = np.round(midpoint + 1, decimals)
-
-        rangeslider.setHigh(midpoint)
-        rangeslider.setLow(lowpoint)
-
-        assert(rangeslider.low == lowpoint)
-        assert(rangeslider.high == highpoint)
-
+#
+# def test_doubleslider(qtbot):
+#     """
+#     test that the doubleslider accurately represents floats
+#     """
+#     doubleslider = widgets.components.DoubleSlider(decimals=decimals)
+#     doubleslider.show()
+#     qtbot.addWidget(doubleslider)
+#
+#     doubleslider.setMinimum(0)
+#     doubleslider.setMaximum(1)
+#
+#     for i in np.random.rand(n_samples):
+#         test_val = np.round(i, decimals)
+#
+#         # set value inside of signal catcher to test both value and signal
+#         with qtbot.waitSignal(doubleslider.doubleValueChanged, timeout=1000) as blocker:
+#             doubleslider.setValue(test_val)
+#
+#         assert(doubleslider.value() == test_val)
+#         assert(blocker.args == test_val)
+#
+#
+# def test_doubleslider_minmax(qtbot, generic_minmax):
+#
+#     doubleslider = widgets.components.DoubleSlider(decimals=decimals)
+#     doubleslider.show()
+#     qtbot.addWidget(doubleslider)
+#
+#     multiplier = 100
+#     for i in range(n_samples):
+#         min, max = generic_minmax()
+#
+#         doubleslider.setMinimum(min)
+#         doubleslider.setMaximum(max)
+#
+#         # test that values were set correctly
+#         assert(doubleslider.minimum() == min)
+#         assert(doubleslider.maximum() == max)
+#
+#         # test below min and above max
+#         test_min = min - np.random.rand()*multiplier
+#         test_max = max + np.random.rand()*multiplier
+#
+#         doubleslider.setValue(test_min)
+#         assert(doubleslider.value() == doubleslider.minimum())
+#         doubleslider.setValue(test_max)
+#         assert(doubleslider.value() == doubleslider.maximum())
+#
+# #################
+# # RangeSlider
+#
+# def test_rangeslider(qtbot, generic_minmax, generic_saferange):
+#     abs_range = generic_minmax()
+#     safe_range = generic_saferange()
+#     print(safe_range)
+#     orientation = QtCore.Qt.Orientation.Horizontal
+#
+#
+#     rangeslider = widgets.components.RangeSlider(
+#         abs_range,
+#         safe_range,
+#         decimals=decimals,
+#         orientation=orientation)
+#     rangeslider.show()
+#     qtbot.addWidget(rangeslider)
+#
+#     # test ranges & values
+#     assert(rangeslider.minimum() == abs_range[0])
+#     assert(rangeslider.maximum() == abs_range[1])
+#     assert(rangeslider.low == safe_range[0])
+#     assert(rangeslider.high == safe_range[1])
+#
+#     for i in range(n_samples):
+#         min, max = generic_saferange()
+#         print(min, max)
+#
+#         with qtbot.waitSignal(rangeslider.valueChanged, timeout=1000) as blocker:
+#             rangeslider.setHigh(max)
+#             assert(rangeslider.high == max)
+#
+#             rangeslider.setLow(min)
+#             assert(rangeslider.low == min)
+#
+# def test_rangeslider_minmax(qtbot, generic_minmax, generic_saferange):
+#     abs_range = generic_minmax()
+#     safe_range = generic_saferange()
+#     decimals = 5
+#     orientation = QtCore.Qt.Orientation.Horizontal
+#
+#     rangeslider = widgets.components.RangeSlider(
+#         abs_range,
+#         safe_range,
+#         decimals=decimals,
+#         orientation=orientation)
+#     rangeslider.show()
+#     qtbot.addWidget(rangeslider)
+#
+#     for i in range(n_samples):
+#         abs_min, abs_max = generic_minmax()
+#         safe_min, safe_max = generic_saferange()
+#
+#         #pdb.set_trace()
+#         # Set min and max and test they were set correctly
+#         rangeslider.setMinimum(abs_min)
+#         assert(rangeslider.minimum() == abs_min)
+#
+#         rangeslider.setMaximum(abs_max)
+#         assert(rangeslider.maximum() == abs_max)
+#
+#         # set low and high and test they were set correctly
+#         rangeslider.setHigh(safe_max)
+#         assert(rangeslider.high == safe_max)
+#
+#         rangeslider.setLow(safe_min)
+#         assert(rangeslider.low == safe_min)
+#
+#         # try to set low and high outside of max
+#         rangeslider.setHigh(abs_max + 1)
+#         assert(rangeslider.high == rangeslider.maximum())
+#
+#         rangeslider.setLow(abs_min - 1)
+#         assert(rangeslider.low == rangeslider.minimum())
+#
+#         # try to set low higher and high and vice versa
+#         midpoint = np.round(np.mean([abs_min, abs_max]), decimals)
+#         highpoint = np.round(midpoint-1, decimals)
+#         lowpoint = np.round(midpoint-1-(10**-decimals), decimals)
+#
+#         rangeslider.setLow(midpoint)
+#         rangeslider.setHigh(highpoint)
+#
+#         assert(rangeslider.high == highpoint)
+#         assert(rangeslider.low == lowpoint)
+#
+#         highpoint = np.round(midpoint+1+(10**-decimals), decimals)
+#         lowpoint = np.round(midpoint + 1, decimals)
+#
+#         rangeslider.setHigh(midpoint)
+#         rangeslider.setLow(lowpoint)
+#
+#         assert(rangeslider.low == lowpoint)
+#         assert(rangeslider.high == highpoint)
+#

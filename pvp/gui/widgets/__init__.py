458d4f35fb80d4cce6234d9e485076e2a03c3fd3
Jonny Saunders <JLSaunders987@gmail.com>
2020-08-12 03:27:03 -0700

    Checking in GUI tests and some healthy code cleanup (#135)
    * working on tests -- just wanna look at coverage report to see what's missing
    
    * coveralls always!
    
    * working on some code coverage of main gui widget
    
    * coordinator doesn't kill controller on stop...
    
    * we got too soft on em, actually killing when closing gui
    
    also explicitly testing VTE since its just a lil different as it doesnt have a corresponding control in the controller
    
    * killing dead code
    
    - RIP the ideal waveform plot it was a good idea ahead of its time.
    - RIP the rangeslider which should be built into Qt but we had to do hand to hand fucking pixelwise drawing for like a week to make. it still was sorta shitty so good riddance
    - Welcome back to life tests for doubleslider
    
    * howd this lil bastard slip in here
    
    (also actually really deleting the rangeslider)

--- pvp/gui/widgets/__init__.py
+++ pvp/gui/widgets/__init__.py
@@ -1,5 +1,5 @@
 from pvp.gui.widgets.display import Display
-from pvp.gui.widgets.plot import Plot, Pressure_Waveform
+from pvp.gui.widgets.plot import Plot, Plot_Container
 from pvp.gui.widgets.control_panel import Control_Panel
 from pvp.gui.widgets.alarm_bar import Alarm_Bar
 from pvp.gui.widgets import components

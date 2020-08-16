486c95cbfac031e47c94be497c7e0a2390c76b63
sneakers-the-rat <JLSaunders987@gmail.com>
2020-08-15 20:08:36 -0700

    mock import

--- _docs/conf.py
+++ _docs/conf.py
@@ -18,6 +18,7 @@ sys.path.insert(0, os.path.abspath('../pvp'))
 sys.path.insert(0, os.path.abspath('.'))
 
 sys.modules['Shiboken'] = mock.Mock()
+sys.modules['PySide2.QtMultimedia'] = mock.Mock()
 
 
 # -- Project information -----------------------------------------------------

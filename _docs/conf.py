2d671b566e39038051a841bc9c656b3c8f2dfbab
sneakers-the-rat <JLSaunders987@gmail.com>
2020-08-16 23:35:19 -0700

    importing correctly

--- _docs/conf.py
+++ _docs/conf.py
@@ -46,7 +46,7 @@ extensions = [
     'sphinx.ext.intersphinx', # include documentation from other projects
     'sphinx.ext.todo',        # todo directive
     'sphinx.ext.viewcode',
-    'sphinx.ext.inheritance-diagram',
+    'sphinx.ext.inheritance_diagram',
     # 'sphinx_automodapi.automodapi',
     'sphinxcontrib.napoleon', # parse google style docstrings
     'autodocsumm',
